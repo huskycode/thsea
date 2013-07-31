c8bd0f809f63603132e5537792c4a8587cbcbccc
Sunai Sukanake <sunai.sukanake@gmail.com>
2013-08-01 02:41:18 +0700

    remove old comment

--- public/protected/views/site/pages/about.php
+++ public/protected/views/site/pages/about.php
@@ -6,12 +6,6 @@ $this->breadcrumbs=array(
 	'About',
 );
 ?>
-<!--
-<h1>About</h1>
-
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
--->
 	<!-- START SEPARATOR  -->
 	<div id="separator">
 		<div class="btop-1px"></div>

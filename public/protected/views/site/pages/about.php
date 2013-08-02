3e35f7a1da1ad47021473a4f71646a475dee7479
Sunai Sukanake <sunai.sukanake@gmail.com>
2013-08-03 01:39:19 +0700

    Test Replace About by Contribute page

--- public/protected/views/site/pages/about.php
+++ public/protected/views/site/pages/about.php
@@ -1,44 +1,174 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - About';
-$this->breadcrumbs=array(
-	'About',
+$this->pageTitle = Yii::app()->name . ' - Video';
+$this->breadcrumbs = array(
+    'Video',
 );
 ?>
-	<!-- START SEPARATOR  -->
-	<div id="separator">
-		<div class="btop-1px"></div>
-		<div class="container">
-			<!-- start separator -->
-			<div class="sixteen columns">
-				<h4 class="page-title">About</h4>
-			</div><!-- sixteen columns -->
-		</div><!-- .container -->
-		<div class="bbottom-1px"></div>
-	</div><!-- #separator -->
-	<!-- END SEPARATOR -->	
-	
-	<!-- START PAGE WRAPPER -->
-	<div class="container main-wrapper">
-		<div id="main-content" class="twelve columns">
-			<img src="images/blog/img-post01.jpg" alt="" class="border alignright" title="Align Right" width="200" />
-			<p>Software Engineering หรือ วิศวกรรมซอฟต์แวร์ คือ การศึกษาที่เกี่ยวกับปัญหาและแนวทางการแก้ไขปัญหาต่างๆ ที่เกิดขึ้นกับการพัฒนาระบบเทคโนโลยีสารสนเทศ โดยเฉพาะซอฟต์แวร์ในองค์กร ที่ผ่านมารายงานด้านปัญหาต่างๆที่เกิดขึ้น โดยการศึกษาในต่างประเทศพบว่าโอกาสที่โครงการพัฒนาซอฟต์แวร์จะประสบความสำเร็จนั้นมีเพียงประมาณ 20-30 % เท่านั้น ซึ่งอัตราส่วนความสำเร็จดังกล่าวแทบจะไม่ได้รับการแก้ไขได้เลยในระยะเวลา 4-5 ปีที่ผ่านมาของการทำวิจัย ไม่ว่าเราจะมีความก้าวหน้าด้านเทคโนโลยีไปมากแค่ไหนก็ตาม</p>
-			<p>จากปัญหาดังกล่าวได้มีกลุ่มผู้ประกอบการ กลุ่มผู้เชี่ยวชาญที่อยู่ในทั้งบริษัทเอกชน และในมหาวิทยาลัยหลายๆแห่งทั่วโลกได้พยายามแก้ไขปัญหา โดยการเสนอแนวทางและเครื่องมือต่างๆ สิ่งที่น่าสนใจคือไม่ว่าจะมีผลงานวิขัยถูกเสนอมามากแค่ไหน ก็ไม่สามารถที่จะแก้ไขปัญหาให้ลดลงอย่างที่เราเห็นในสถิติที่ผ่านมา</p>
-			
-			<!-- QUOTE -->
-			<div class="row">
-				<div class="four columns alpha">
-					<blockquote>การศึกษาในต่างประเทศพบว่าโอกาสที่โครงการพัฒนาซอฟต์แวร์จะประสบความสำเร็จนั้นมีเพียงประมาณ 20-30% เท่านั้น</blockquote>
-				</div>
-				<div class="eight columns omega">
-					<p>Thailand Software Engineering Group เป็นการรวมตัวกันของบริษัทและบุคคลที่ทำงานจริงในการพัฒนาระบบต่างๆของประเทศไทย เพื่อที่จะเป็นแหล่งรวบรวมบุคลากรที่ทำงานทางการพัฒนาซอฟต์แวร์ และวิจัยด้านนี้ของประเทศ การรวมตัวนี้ก็เพื่อการแลกเปลี่ยนข้อมูลประสบการณ์ความรู้และผลงานวิจัยต่างๆ ที่ถูกคิดค้นขึ้นมาเพื่อประโยชน์ต่อการพัฒนาระบบสารสนเทศภายในประเทศ โดยเฉพาะอย่างยิ่งเพื่อเป็นแหล่งฝึกอบรมด้านต่างๆ โดยคณาจารย์ที่เกิดจากการรวมตัวกันของผู้ที่ทำงานอยู่จริงจากองค์กรต่างๆ ทั้งในและต่างประเทศอาทิเช่น Sofrware Park บริษัท Microsoft บริษัท IBM บริษัท Agoda บริษัท Nokia ประเทศไทย บริษัท Thomson Reuters บริษัท TRUE บริษัท AIIT และบริษัท PKT รวมไปถึงกลุ่มผู้สนใจจาก community ต่างๆ เช่น Agile66 และ WeLoveBug เป็นต้น</p>
-				</div>
-			</div>
-			<!-- QUOTE -->
-			<div class="divider-page-1px"></div>			
-					
-		</div><!-- main-content -->
-	</div><!-- .container -->
+<!-- START SEPARATOR  -->
+<div id="separator">
+    <div class="btop-1px"></div>
+    <div class="container">
+        <!-- start separator -->
+        <div class="sixteen columns">
+            <h4 class="page-title">The Video</h4>
+        </div><!-- sixteen columns -->
+    </div><!-- .container -->
+    <div class="bbottom-1px"></div>
+</div><!-- #separator -->
+<!-- END SEPARATOR -->	
+<!-- START BLOG WRAPPER -->
+<div class="container main-wrapper">
+    <div id="main-content" class="twelve columns">
 
-		<!-- END BLOG WRAPPER -->
+        <!-- POST #3 VIDEO -->
+        <div class="entry-post format-video">
+            <div class="info-post">
+            </div><!-- info-post -->
+
+            <div class="stack">
+                <div class="meta-post">
+                    <div class="date" title="Posted Date"><span>Jul 20, 2013</span></div>
+                    <div class="tags" title="Tags"><span><a href="#">AgileThailand</a>, <a href="#">2013</a></span></div>
+                </div><!-- meta-post -->
+            </div><!-- stack -->
+            <div class="image-post">
+                <iframe width="654" height="368" src="//www.youtube.com/embed/gVaRj5GgOPg" frameborder="0"></iframe>
+            </div><!-- post-image -->
+            <div class="text-post clearfix">
+                <div class="title-post">
+                    <h6><a href="blog-post.html">Agile Thailand 2013 : วิกฤติโปรแกรมเมอร์ไทย แก้ได้ด้วยอไจล์</a></h6>	
+                </div>
+                <p>จาก "แบไต๋ไฮเทค: Special Report - วิกฤตการณ์โปรแกรมเมอร์ไทย" ได้เกิดการถกเถียงกันอย่างมากใน Facebook Group ของ Agile66 ถึงสาเหตุที่แท้จริงของปัญหา รวมไปถึงหนทางที่จะแก้ไข เราจึงพูดคุยกันว่าเราจะเอาเรื่องนี้มาคุย­กันใน กิจกรรมประจำปีของชาว Agile ที่มีชื่อว่า Agile Thailand 2013 ทำให้เกิดเป็น Panel Discussion Session ดราม่าแห่งปี ในชื่อ "วิกฤติโปรแกรมเมอร์ไทย แก้ได้ด้วยอไจล์"</p>
+            </div><!-- text-post -->				
+            <div class="divider-blog-1px"></div>
+        </div><!-- entry-post -->
+
+        <!-- POST #3 VIDEO -->
+        <div class="entry-post format-video">
+            <div class="info-post">
+            </div><!-- info-post -->
+
+            <div class="stack">
+                <div class="meta-post">
+                    <div class="date" title="Posted Date"><span>Jul 20, 2013</span></div>
+                    <div class="tags" title="Tags"><span><a href="#">AgileThailand</a>, <a href="#">2013</a></span></div>
+                </div><!-- meta-post -->
+            </div><!-- stack -->
+            <div class="image-post">
+                <iframe width="654" height="368" src="//www.youtube.com/embed/qYxFmmV0eA0" frameborder="0"></iframe>				   	
+            </div><!-- post-image -->
+            <div class="text-post clearfix">
+                <div class="title-post">
+                    <h6><a href="blog-post.html">Agile Thailand 2013 : Agile in Startup</a></h6>	
+                </div>
+                <p>No description avaiable</a>
+            </div><!-- text-post -->				
+            <div class="divider-blog-1px"></div>
+        </div><!-- entry-post -->
+
+
+        <!-- POST #3 VIDEO -->
+        <div class="entry-post format-video">
+            <div class="info-post">
+            </div><!-- info-post -->
+
+            <div class="stack">
+                <div class="meta-post">
+                    <div class="date" title="Posted Date"><span>Jul 20, 2013</span></div>
+                    <div class="tags" title="Tags"><span><a href="#">AgileThailand</a>, <a href="#">2013</a></span></div>
+                </div><!-- meta-post -->
+            </div><!-- stack -->
+            <div class="image-post">
+                <iframe width="654" height="368" src="//www.youtube.com/embed/ZM7PYrJ0MCE" frameborder="0"></iframe>				   	
+            </div><!-- post-image -->
+            <div class="text-post clearfix">
+                <div class="title-post">
+                    <h6><a href="blog-post.html">Agile Thailand 2013 : Agile vs Lean</a></h6>	
+                </div>
+                <p>Welcome to the lean entrepreneur</a>
+            </div><!-- text-post -->				
+            <div class="divider-blog-1px"></div>
+        </div><!-- entry-post -->
+
+        <!-- POST #3 VIDEO -->
+        <div class="entry-post format-video">
+            <div class="info-post">
+            </div><!-- info-post -->
+
+            <div class="stack">
+                <div class="meta-post">
+                    <div class="date" title="Posted Date"><span>Jul 20, 2013</span></div>
+                    <div class="tags" title="Tags"><span><a href="#">AgileThailand</a>, <a href="#">2013</a></span></div>
+                </div><!-- meta-post -->
+            </div><!-- stack -->
+            <div class="image-post">
+                <iframe width="654" height="368" src="//www.youtube.com/embed/aD6eYjK36fA" frameborder="0"></iframe>				   	
+            </div><!-- post-image -->
+            <div class="text-post clearfix">
+                <div class="title-post">
+                    <h6><a href="blog-post.html">Agile Thailand 2013 : Kamon's Rules</a></h6>	
+                </div>
+                <p>สร้างกฏไว้เยอะเกินจนงง ดู NCIS แล้วเค้ามี Gibbs' Rules เลยคิดว่าต้องทำบ้างแล้ว http://korn4d.wordpress.com/kamon-rules/</a>
+            </div><!-- text-post -->				
+            <div class="divider-blog-1px"></div>
+        </div><!-- entry-post -->
+
+
+        <!-- POST #3 VIDEO -->
+        <!--<div class="entry-post format-video">-->
+        <!--	<div class="info-post">-->
+        <!--		<div class="ribbon-wrapper">-->
+        <!--			<div class="ribbon-front">-->
+        <!--				<a href="blog-post.html" title="Video Post Type"><span class="blog-post-format"></span></a>-->
+        <!--			</div>-->
+        <!--			<div class="ribbon-edge-bottomleft"></div>-->
+        <!--</div>-->
+        <!--</div>-->
+
+        <!--	<div class="stack">-->
+        <!--		<div class="meta-post">-->
+        <!--			<div class="date" title="Posted Date"><span>May 19, 2012</span></div>-->
+        <!--			<div class="tags" title="Tags"><span><a href="#">Business</a>, <a href="#">Chart</a></span></div>-->
+        <!--			<div class="comments" title="Comments"><span><a href="#">5</a></span></div>-->
+        <!--			<div class="author" title="Author"><span><a href="#">Slimbet</a></span></div>-->
+                                <!--<div class="tweets" title="Tweet this"><span><a href="https://twitter.com/intent/tweet?text=Check%20out%20this%20Video&amp;url=http://html.color-theme.com/photomin/blog.html&amp;via=zesky">Tweet</a></span></div>-->
+        <!--</div>-->
+        <!--</div>-->
+        <!--	<div class="image-post">-->
+        <!--	   	<iframe src="http://player.vimeo.com/video/40255993?title=0&amp;byline=0&amp;portrait=0&amp;color=ff7e00" width="654" height="368"></iframe>-->
+        <!--</div>-->
+        <!--	<div class="text-post clearfix">-->
+        <!--			<div class="title-post">-->
+        <!--			<h6><a href="blog-post.html">In Particles (Video)</a></h6>	-->
+        <!--			</div>-->
+        <!--	   	<p>Adobe After Effects project file. You can buy it from here: <a href="http://videohive.net/item/in-particles/2092596?ref=zerge">videohive.net/item/in-particles/2092596</a></p>-->
+        <!--		<a href="blog-post.html" class="button read-more">Read more</a>-->
+        <!--</div>				-->
+        <!--	<div class="divider-blog-1px"></div>-->
+        <!--</div>-->
+
+
+        <!-- START PAGINATION-->
+        <div id="nav-pagination">
+            <ul class="nav-pagination clearfix">
+                <li class="first"><a href="#"></a></li>
+                <li clase="current"><a href="#">1</a></li>
+                <!--<li class="current"><a href="#">2</a></li>-->
+                <!--<li><a href="#">3</a></li>-->
+                <!--<li><a href="#">4</a></li>-->
+                <!--<li><a href="#">5</a></li>-->
+                <!--<li><a href="#">6</a></li>-->
+                <!--<li><a href="#">7</a></li>-->
+                <li class="last"><a href="#"></a></li>
+            </ul>
+        </div><!-- #nav-pagination -->
+    </div><!-- main-content -->
+
+    <!-- START SIDEBAR -->
+    <div id="sidebar" class="four columns">	
+    </div><!-- sidebar -->
+</div><!-- .container -->
+
+<!-- END BLOG WRAPPER -->

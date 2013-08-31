ea51fdf19c7ff8330cb810d7a357bc643ff9eb1f
ruxcom <ruxcom@hotmail.com>
2013-09-01 03:28:53 +0700

    Change database connection string for UAT

--- public/protected/config/main.php
+++ public/protected/config/main.php
@@ -49,7 +49,7 @@ return array(
 		*/
                 
 		'db'=>array(
-			'connectionString' => 'mysql:host=thsea.db;dbname=thsea',
+			'connectionString' => 'mysql:host=thsea.db;dbname=thsea_uat',
 			'emulatePrepare' => true,
 			'username' => 'thsea_admin',
 			'password' => 'andaman',

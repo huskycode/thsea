a2ca0929d0787cf31b7f634cf69fb5dc4eac3285
ruxcom <ruxcom@hotmail.com>
2013-09-07 21:55:23 +0700

    Add database structure and data

---
+++ db/thsea-data.sql
@@ -0,0 +1,30 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.5.29 : Database - thsea
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`thsea` /*!40100 DEFAULT CHARACTER SET tis620 */;
+
+USE `thsea`;
+
+/*Data for the table `video` */
+
+insert  into `video`(`id`,`title`,`description`,`url`,`recording_date`,`posted_date`,`posted_by`) values (4143,'Agile Thailand 2013 : วิกฤติโปรแกรมเมอร์ไทย แก้ได้ด้วยอไจล','จาก \"แบไต๋ไฮเทค: Special Report - วิกฤตการณ์โปรแกรมเมอร์ไทย\" ได้เกิดการถกเถียงกันอย่างมากใน Facebook Group ของ Agile66 ถึงสาเหตุที่แท้จริงของปัญหา รวมไปถึงหนทางที่จะแก้ไข เราจึงพูดคุยกันว่าเราจะเอาเรื่องนี้มาคุย?กันใน กิจกรรมประจำปีของชาว Agile ที่มีชื่อว่า Agile Thailand 2013 ทำให้เกิดเป็น Panel Discussion Session ดราม่าแห่งปี ในชื่อ \"วิกฤติโปรแกรมเมอร์ไทย แก้ได้ด้วยอไจล์\"','http://youtu.be/gVaRj5GgOPg',NULL,'2013-07-20 00:00:00','admin'),(4144,'Agile Thailand 2013 : Agile in Startup',NULL,'http://youtu.be/qYxFmmV0eA0',NULL,'2013-07-20 00:00:00','admin'),(4145,'Agile Thailand 2013 : Agile vs Lean','Welcome to the lean entrepreneur','http://youtu.be/ZM7PYrJ0MCE',NULL,'2013-07-20 00:00:00','admin'),(4146,'Agile Thailand 2013 : Kamon\'s Rules','สร้างกฏไว้เยอะเกินจนงง ดู NCIS แล้วเค้ามี Gibbs\' Rules เลยคิดว่าต้องทำบ้างแล้ว http://korn4d.wordpress.com/kamon-rules','http://youtu.be/aD6eYjK36fA',NULL,'2013-07-20 00:00:00','admin');
+
+/*Data for the table `video_tag` */
+
+insert  into `video_tag`(`video_id`,`tag`) values (4143,'2013'),(4143,'Agile Thailand'),(4144,'2013'),(4144,'Agile Thailand'),(4145,'2013'),(4145,'Agile Thailand'),(4146,'2013'),(4146,'Agile Thailand');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ db/thsea-structure.sql
@@ -0,0 +1,48 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.5.29 : Database - thsea
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`thsea` /*!40100 DEFAULT CHARACTER SET tis620 */;
+
+USE `thsea`;
+
+/*Table structure for table `video` */
+
+DROP TABLE IF EXISTS `video`;
+
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Video ID',
+  `title` varchar(200) NOT NULL COMMENT 'Video title',
+  `description` varchar(2000) DEFAULT NULL COMMENT 'Video description',
+  `url` varchar(500) NOT NULL COMMENT 'Video url',
+  `recording_date` date DEFAULT NULL COMMENT 'Recording date',
+  `posted_date` datetime NOT NULL COMMENT 'Posted date',
+  `posted_by` varchar(50) NOT NULL COMMENT 'Posted by',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4147 DEFAULT CHARSET=tis620 COMMENT='Video';
+
+/*Table structure for table `video_tag` */
+
+DROP TABLE IF EXISTS `video_tag`;
+
+CREATE TABLE `video_tag` (
+  `video_id` int(11) NOT NULL COMMENT 'Video ID',
+  `tag` varchar(50) NOT NULL COMMENT 'tag',
+  PRIMARY KEY (`video_id`,`tag`),
+  CONSTRAINT `FK_video_tag` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=tis620 COMMENT='Video tag';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

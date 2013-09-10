5460b3d3b51ae5b22d3329e610f018c26f9b79b2
ruxcom <ruxcom@hotmail.com>
2013-09-11 00:10:06 +0700

    Saperate db connection string to external file (db.php).

--- public/protected/config/console.php
+++ public/protected/config/console.php
@@ -3,35 +3,40 @@
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
-
-	// preloading 'log' component
-	'preload'=>array('log'),
-
-	// application components
-	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-			),
-		),
-	),
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'My Console Application',
+    // preloading 'log' component
+    'preload' => array('log'),
+    'commandMap' => array(
+        'migrate' => array(
+            'class' => 'system.cli.commands.MigrateCommand',
+            'migrationPath' => 'application.migrations',
+            'migrationTable' => 'tbl_migration',
+            'connectionID' => 'db',
+        /* 'templateFile'=>'application.migrations.template', */
+        ),
+    ),
+    // application components
+    'components' => array(
+        'db' => require(dirname(__FILE__).'/db.php'),
+        // uncomment the following to use a MySQL database
+        /*
+          'db'=>array(
+          'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+          'emulatePrepare' => true,
+          'username' => 'root',
+          'password' => '',
+          'charset' => 'utf8',
+          ),
+         */
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+            ),
+        ),
+    ),
 );

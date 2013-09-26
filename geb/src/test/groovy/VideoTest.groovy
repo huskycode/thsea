04dea54b852dff4fdab42326ecea24c18cb62c82
Narathip <narathip@mintedimages.com>
2013-09-26 23:11:03 +0700

    update video test

--- geb/src/test/groovy/VideoTest.groovy
+++ geb/src/test/groovy/VideoTest.groovy
@@ -6,29 +6,20 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 class VideoTest extends GebReportingTest {
-
-    @Test
-    void theTitleLinkShouldBeThSEA() {
-        to SEacademyPage
-        
-	    assert title == "Thailand Software Engineering Academy"	
-    }
     
     @Test
     void locationChangeWithHashTag(){
         Browser.drive {
-            go "http://uat.seacademy.in.th/video"
-
+        to VideoPage
             // make sure we actually got to the page
             assert title == "Thailand Software Engineering Academy - Video"
-
 
             // is the first link to wikipedia?
             def videoLink = $("span.fb_comments_count", 0)
             // click the link 
             videoLink.click()
             // wait for Google's javascript to redirect to Wikipedia
-            waitFor { title == "Wikipedia" }
+            assert getCurrentWindow() != "http://uat.seacademy.in.th/video"
         }
     }
 }
---
+++ geb/src/test/groovy/pages/VideoPage.groovy
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import geb.*
+
+/**
+ *
+ * @author Gong
+ */
+class VideoPage extends Page  {
+    static url = "http://uat.seacademy.in.th/video"
+    static at = { title == "Thailand Software Engineering Academy - Video" }
+}
+

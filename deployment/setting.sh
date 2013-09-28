aa7ff9f0e9c201d05783c257768f58375d789acd
Sunai Sukanake <sunai.sukanake@gmail.com>
2013-09-28 17:46:36 +0700

    add setting shell script

---
+++ deployment/setting.sh
@@ -0,0 +1,17 @@
+## intial destination
+if [ "$1" = "uat" ]
+then 
+     DESTINATION="varokas_thsea-uat@ssh.phx.nearlyfreespeech.net"   
+elif [ "$1" = "prod" ] 
+then 
+     DESTINATION="varokas_thsea@ssh.phx.nearlyfreespeech.net"
+else
+     echo "wrong command"
+fi
+
+## setting deployment folder permission for Yii framework
+##  writable "runtime&assets" folder
+echo "ssh -i /home/thsea/.ssh/id_rsa $DESTINATION \"chmod -R 777 ./protected/runtime\""
+echo "ssh -i /home/thsea/.ssh/id_rsa $DESTINATION \"chmod -R 777 ./assets\""
+##  Change group for WEB user
+echo "ssh -i /home/thsea/.ssh/id_rsa $DESTINATION \"chgrp -R web *\""

293aeaea68f1eb6d24b6dcf8605ce3cad7cb48d1
Narathip <narathip@mintedimages.com>
2013-09-30 23:54:15 +0700

    update test video page

--- geb/src/test/groovy/VideoTest.groovy
+++ geb/src/test/groovy/VideoTest.groovy
@@ -4,22 +4,16 @@ import org.junit.Test
 
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-
+@RunWith(JUnit4)
 class VideoTest extends GebReportingTest {
-    
     @Test
     void locationChangeWithHashTag(){
-        Browser.drive {
         to VideoPage
-            // make sure we actually got to the page
-            assert title == "Thailand Software Engineering Academy - Video"
-
-            // is the first link to wikipedia?
-            def videoLink = $("span.fb_comments_count", 0)
-            // click the link 
-            videoLink.click()
-            // wait for Google's javascript to redirect to Wikipedia
-            assert getCurrentWindow() != "http://uat.seacademy.in.th/video"
-        }
+        // make sure we actually got to the page
+        assert title == "Thailand Software Engineering Academy - Video"
+        // click the link 
+        $("a.fb-comment-count", 0).click();
+        // wait for Google's javascript to redirect to Wikipedia
+        assert browser.driver.executeScript("return window.location.href;") == $("a.fb-comment-count", 0).@href;
     }
 }

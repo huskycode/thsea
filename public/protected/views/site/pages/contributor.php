142acacd90ccc6edf13e61ebba6a6c13b24c6a51
Narathip <narathip@mintedimages.com>
2013-10-09 01:34:38 +0700

    update font size and fix in contributor page

--- public/protected/views/site/pages/contributor.php
+++ public/protected/views/site/pages/contributor.php
@@ -21,80 +21,163 @@ $this->breadcrumbs = array(
 
 <!-- START PAGE WRAPPER -->
 <div class="container main-wrapper">
-    <div id="main-content" class="sixteen columns">
+    <div id="main-content">
         <p>ทีมงานผู้จัดทำเว็บไซต์Thailand Software Engineering Academy เป็นกลุ่มคนที่สนใจจะสร้างเว็บไซต์และได้นำScrumมาใช้ในการทำงานร่วมกันเพื่อพัฒนาเว็บไซต์นี้ขึ้นมา</p>
 
         <!-- IMAGES -->
+        <div id="container-portfolio" class="portfolio-4">
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a>วโรกาส ภาณุสุวรรณ (นนท์)</a>
+                        <a>Varokas Panusuwan</a>
+                        <a>Product Owner</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/non.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/non.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
+            </div>
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a>อดิสรณ์ โชคอำนวย (อาร์ม)</a>
+                        <a>Adisorn Chockaumnuai (Arm)</a>
+                        <a>Scrum Master</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/arm.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/arm.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
+            </div>
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a>กรัณย์ ศิวารัตน์ (บอมบ์)</a>
+                        <a>Karan Sivarat (Bomb)</a>
+                        <a>Scrum Master</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/bomb.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/bomb.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
+            </div>
 
-        <div class="row">				   	
-            <div class="four-column ">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/non.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/non.jpg" alt="" class="border " /></a>
-                <p class="description"> วโรกาส ภาณุสุวรรณ (นนท์)</p>
-                <p class="description">Varokas Panusuwan</p>
-                <p class="description">Product Owner</p>
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a class="description">พิษณุ สว่างแผ้ว(รุ่ง)</a>
+                        <a class="description">Pitsanu Swangpheaw (Roong)</a>
+                        <a class="description">Technical Consultant</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/roong.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/roong.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
             </div>
-            <div class="four-column ">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/arm.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/arm.jpg" alt="" class="border " /></a>
-                <p class="description">อดิสรณ์ โชคอำนวย (อาร์ม)</p>
-                <p class="description">Adisorn Chockaumnuai (Arm)</p>
-                <p class="description">Scrum Master</p>
+
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a class="description">นิติศักดิ์  มูลตรีศรี (มุ้ย)</a>
+                        <a class="description">Nitisak Mooltreesri (Mui)</a>
+                        <a class="description">Scrum Master</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/mui.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/mui.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
             </div>
-            <div class="four-column">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/bomb.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/bomb.jpg" alt="" class="border " /></a>
-                <p class="description">กรัณย์ ศิวารัตน์ (บอมบ์)</p>
-                <p class="description">Karan Sivarat (Bomb)</p>
-                <p class="description">Scrum Master</p>
+
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a class="description">นราธิป หริจิระติวงศ์(ก้อง)</a>
+                        <a class="description">Narathip Harijiratiwong (Kong)</a>
+                        <a class="description">Developer</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/kong.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/kong.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
             </div>
-            <div class="four-column">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/roong.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/roong.jpg" alt="" class="border " /></a>
-                <p class="description">พิษณุ สว่างแผ้ว(รุ่ง)</p>
-                <p class="description">Pitsanu Swangpheaw (Roong)</p>
-                <p class="description">Technical Consultant</p>
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a class="description">วรัญช์ เจริญสุข (ออฟ)</a>
+                        <a class="description">Warun Chareonsuk (Off)</a>
+                        <a class="description">Developer</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/off.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/off.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
             </div>
-            <div class="four-column column-last">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/mui.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/mui.jpg" alt="" class="border " /></a>
-                <p class="description">นิติศักดิ์  มูลตรีศรี (มุ้ย)</p>
-                <p class="description">Nitisak Mooltreesri (Mui)</p>
-                <p class="description">Scrum Master</p>
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a class="description">เจนจิรา วงศ์บุญสิน (ปลา)</a>
+                        <a class="description">Jenjira Wongboonsin (Pla)</a>
+                        <a class="description">Developer</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/pla.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/pla.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
             </div>
-        </div>
-        <div class="row">
-            <div class="four-column ">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/kong.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/kong.jpg" alt="" class="border " /></a>
-                <p class="description">นราธิป หริจิระติวงศ์(ก้อง)</p>
-                <p class="description">Narathip Harijiratiwong (Kong)</p>
-                <p class="description">Developer</p>
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a class="description">สุนัย สุขเอนก (นุก)</a>
+                        <a class="description">Sunai Sukanake (Nook)</a>
+                        <a class="description">Developer</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/nook.png" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/nook.png" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
             </div>
-            <div class="four-column ">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/off.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/off.jpg" alt="" class="border " /></a>
-                <p class="description">วรัญช์ เจริญสุข (ออฟ)</p>
-                <p class="description">Warun Chareonsuk (Off)</p>
-                <p class="description">Developer</p>
-            </div>
-            <div class="four-column ">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/pla.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/pla.jpg" alt="" class="border " /></a>
-                <p class="description">เจนจิรา วงศ์บุญสิน (ปลา)</p>
-                <p class="description">Jenjira Wongboonsin (Pla)</p>
-                <p class="description">Developer</p>
-            </div>
-            <div class="four-column">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/nook.png"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/nook.png" alt="" class="border " /></a>
-                <p class="description">สุนัย สุขเอนก (นุก)</p>
-                <p class="description">Sunai Sukanake (Nook)</p>
-                <p class="description">Developer</p>
-            </div>
-             <div class="four-column column-last">
-                <a class="zoom" href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/rux.jpg"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/rux.jpg" alt="" class="border " /></a>
-                <p class="description">ธีระพล ม่วงยัง (รักษ์)</p>
-                <p class="description">Teerapon Muangyoung (Rux)</p>
-                <p class="description">Developer</p>
+
+            <div class="item-block four columns photography">
+                <div class="stack">
+                    <div class="title">
+                        <a class="description">ธีระพล ม่วงยัง (รักษ์)</a>
+                        <a class="description">Teerapon Muangyoung (Rux)</a>
+                        <a class="description">Developer</a>
+                    </div>
+                </div><!-- stack -->
+                <div class="block">
+                    <div class="mask-img"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/rux.jpg" alt="" class="border " /></div>
+                    <div class="mask"><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/contributor/rux.jpg" data-rel="zoom-img" class="zoom-icon"></a></div>
+                </div><!-- block -->
             </div>
         </div>
         <!-- IMAGES -->
-
-
-
     </div><!-- main-content -->
 </div><!-- .container -->
 
 <!-- END BLOG WRAPPER -->
+
+<script>
+    /***************************************************
+     ISOTOP - PORTFOLIO
+     ***************************************************/
+    jQuery.noConflict()(function($) {
+        // cache container
+        var $container = $('#container-portfolio');
+        // initialize isotope
+        $container.isotope({
+            itemSelector: '.item-block',
+            layoutMode: 'fitRows'
+        });
+    });
+</script>

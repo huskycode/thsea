8b76dee06f76043467b27f42255c842089a90e7a
Narathip <narathip@mintedimages.com>
2013-10-24 00:10:08 +0700

    add facebook like button

--- public/protected/views/site/index.php
+++ public/protected/views/site/index.php
@@ -77,7 +77,7 @@ function renderTags($videoTags) {
                 <div class="stack">
                     <div class="meta-post">
                         <div style="right:0; position:absolute;background:none;" class="pull-right">
-                            <!--div class="fb-like" data-href="<?php echo getLikeUrl($row->id); ?>" data-width="200" data-layout="button_count" data-show-faces="false" data-send="false"></div-->
+                            <div class="fb-like" data-href="<?php echo getLikeUrl($row->id); ?>" data-width="200" data-layout="button_count" data-show-faces="false" data-send="false"></div>
                         </div>
                         <div class="date" title="Recording Date"><span><?php echo $row->recording_date != null ? Yii::app()->dateFormatter->formatDateTime($row->recording_date, 'long', null) : '-' ?></span></div>
                         <div class="tags" title="Tags"><span>

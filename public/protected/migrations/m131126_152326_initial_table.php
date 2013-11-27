2f73a38e4565b1c8d4f0866c04a6037aaffaa62a
warun <warun@ubinix.com>
2013-11-27 19:02:09 +0700

    change 'CDbMigration.createTable()' to 'CDbMigration.execute()' for 'IF NOT EXISTS' SQL statement

--- public/protected/migrations/m131126_152326_initial_table.php
+++ public/protected/migrations/m131126_152326_initial_table.php
@@ -4,29 +4,57 @@ class m131126_152326_initial_table extends CDbMigration
 {
 	public function up()
 	{
-	   $this->createTable('user',array(
-             'id'=>'int(11) NOT NULL AUTO_INCREMENT',
-             'user_name'=>'varchar(20) NOT NULL',
-             'password'=>'varchar(40) NOT NULL',
-             'full_name'=>'varchar(50) DEFAULT NULL',
-             'PRIMARY KEY (id)'));
-             
-	   $this->createTable('video',array(
-             'id'=>'int(11) NOT NULL AUTO_INCREMENT COMMENT \'Video ID\'',
-             'title'=>'varchar(200) NOT NULL COMMENT \'Video title\'',
-             'description'=>'varchar(5000) DEFAULT NULL COMMENT \'Video description\'',
-             'url'=>'varchar(500) NOT NULL COMMENT \'Video url\'',
-             'thumbnail_url'=>'varchar(1000) NOT NULL COMMENT \'Thumbnail url\'',
-             'recording_date'=>'date DEFAULT NULL COMMENT \'Recording date\'',
-             'posted_date'=>'datetime NOT NULL COMMENT \'Posted date\'',
-             'posted_by'=>'varchar(50) NOT NULL COMMENT \'Posted by\'',
-             'PRIMARY KEY (id)'));
+
+	    $this->execute("CREATE TABLE IF NOT EXISTS user (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            user_name varchar(20) NOT NULL,
+            password varchar(40) NOT NULL,
+            full_name varchar(50) DEFAULT NULL,
+            PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=tis620;", array());
         
-       $this->createTable('video_tag',array(
-             'video_id'=>'int(11) NOT NULL AUTO_INCREMENT COMMENT \'Video ID\'',
-             'tag'=>'varchar(50) NOT NULL COMMENT \'Video title\'',
-             'PRIMARY KEY (video_id, tag)',
-             'CONSTRAINT FK_video_tag FOREIGN KEY (video_id) REFERENCES video(id) ON DELETE CASCADE ON UPDATE CASCADE'));
+        $this->execute("CREATE TABLE IF NOT EXISTS video (
+            id int(11) NOT NULL AUTO_INCREMENT COMMENT 'Video ID',
+            title varchar(200) NOT NULL COMMENT 'Video title',
+            description varchar(5000) DEFAULT NULL COMMENT 'Video description',
+            url varchar(500) NOT NULL COMMENT 'Video url',
+            thumbnail_url varchar(1000) DEFAULT NULL COMMENT 'Thumbnail url',
+            recording_date date DEFAULT NULL COMMENT 'Recording date',
+            posted_date datetime NOT NULL COMMENT 'Posted date',
+            posted_by varchar(50) NOT NULL COMMENT 'Posted by',
+            PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=4178 DEFAULT CHARSET=tis620 COMMENT='Video'", array());
+
+	    $this->execute("CREATE TABLE IF NOT EXISTS video_tag (
+            video_id int(11) NOT NULL COMMENT 'Video ID',
+            tag varchar(50) NOT NULL COMMENT 'tag',
+            PRIMARY KEY (video_id,tag),
+            CONSTRAINT FK_video_tag FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE ON UPDATE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=tis620 COMMENT='Video tag'", array());
+            
+	   //$this->createTable('user',array(
+    //          'id'=>'int(11) NOT NULL AUTO_INCREMENT',
+    //          'user_name'=>'varchar(20) NOT NULL',
+    //          'password'=>'varchar(40) NOT NULL',
+    //          'full_name'=>'varchar(50) DEFAULT NULL',    
+    //          'PRIMARY KEY (id)'));
+             
+	   //$this->createTable('video',array(
+    //          'id'=>'int(11) NOT NULL AUTO_INCREMENT COMMENT \'Video ID\'',
+    //          'title'=>'varchar(200) NOT NULL COMMENT \'Video title\'',
+    //          'description'=>'varchar(5000) DEFAULT NULL COMMENT \'Video description\'',
+    //          'url'=>'varchar(500) NOT NULL COMMENT \'Video url\'',
+    //          'thumbnail_url'=>'varchar(1000) NOT NULL COMMENT \'Thumbnail url\'',
+    //          'recording_date'=>'date DEFAULT NULL COMMENT \'Recording date\'',
+    //          'posted_date'=>'datetime NOT NULL COMMENT \'Posted date\'',
+    //          'posted_by'=>'varchar(50) NOT NULL COMMENT \'Posted by\'',
+    //          'PRIMARY KEY (id)'));
+        
+    //   $this->createTable('video_tag',array(
+    //          'video_id'=>'int(11) NOT NULL AUTO_INCREMENT COMMENT \'Video ID\'',
+    //          'tag'=>'varchar(50) NOT NULL COMMENT \'Video title\'',
+    //          'PRIMARY KEY (video_id, tag)',
+    //          'CONSTRAINT FK_video_tag FOREIGN KEY (video_id) REFERENCES video(id) ON DELETE CASCADE ON UPDATE CASCADE'));
              
 	}
 

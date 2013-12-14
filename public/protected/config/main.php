dd344e47d2926ccb1366ada743a8ee3b9fc5593c
Sunai Sukanake <sunai.sukanake@gmail.com>
2013-12-14 21:26:10 +0700

    Test success 3 video per page

--- public/protected/config/main.php
+++ public/protected/config/main.php
@@ -98,6 +98,6 @@ return array(
     'params' => array(
         // this is used in contact page
         'adminEmail' => 'webmaster@example.com',
-        'videoListPageSize' => '15'
+        'videoListPageSize' => '3'
     ),
 );

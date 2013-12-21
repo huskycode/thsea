420a280aae08a961f49d87dd661684598bf3a501
ruxcom <ruxcom@hotmail.com>
2013-12-21 12:44:00 +0700

    change email support

--- public/protected/config/main.php
+++ public/protected/config/main.php
@@ -97,7 +97,7 @@ return array(
     // using Yii::app()->params['paramName']
     'params' => array(
         // this is used in contact page
-        'adminEmail' => 'webmaster@example.com',
+        'adminEmail' => 'webmaster@seacademy.in.th',
         'videoListPageSize' => '3'
     ),
 );

682a4e12282c03f8fea94dda680870f46c4c9d2a
Narathip Harijiratiwong <tholop@gmail.com>
2014-01-29 23:18:30 +0700

    add bracket to check video is null

--- public/protected/controllers/ApiController.php
+++ public/protected/controllers/ApiController.php
@@ -20,17 +20,18 @@ class ApiController extends Controller {
 
     public function actionVideo($id) {
         $video = Video::model()->findByPk($id);
-
-        if ($video === null)
+        if ($video === null){
             throw new CHttpException(404, 'The video does not exist.');
+        }
         $this->addViewCounter($id);
         echo CJSON::encode($video);
     }
 
     private function addViewCounter($id) {
         $model = Video::model()->findByPk($id);
-        if ($model === null)
+        if ($model === null){
             throw new CHttpException(404, 'The requested page does not exist.');
+        }
         $model->view_counter = $model->view_counter+1;
         $model->view_counter;
         $model->save();

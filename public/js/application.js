71893a5840953f88491bea7b5997e9069e7a07ae
ruxcom <ruxcom@hotmail.com>
2014-03-08 22:55:05 +0700

    Display video description in the Video List.

--- public/js/application.js
+++ public/js/application.js
@@ -17,6 +17,11 @@ var application = {
                 }
             });
             return videoInfo;
+        },
+            nl2br: function (str, is_xhtml) {
+            var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br ' + '/>' : '<br>'; // Adjust comment to avoid issue on phpjs.org display
+
+            return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
         }
     }
 };

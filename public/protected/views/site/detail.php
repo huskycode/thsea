7b7cf14e8aee10197f57c5115939c9dd68019baa
Narathip Harijiratiwong <tholop@gmail.com>
2014-03-16 22:51:06 +0700

    covert youtube url with regular expression

--- public/protected/views/site/detail.php
+++ public/protected/views/site/detail.php
@@ -4,6 +4,9 @@ $this->pageTitle = Yii::app()->name;
 $this->breadcrumbs = array(
     'Index',
 );
+$search = '#(?:http?://)?(?:www\.)?(?:youtu\.be/|youtube\.com(?:/embed/|/v/|/watch\?v=))([\w\-]{10,12}).*$#x';
+$replace = '//www.youtube.com/embed/$1';
+$url = preg_replace($search, $replace, $video->url);
 ?>
 <div id="fb-root"></div>
 <div id="separator">
@@ -17,8 +20,8 @@ $this->breadcrumbs = array(
                 <div class="fb-like" data-href="<?php echo Yii::app()->request->getBaseUrl(true) . '#fb-like-' . $video->id; ?>" data-width="200" data-layout="button_count" data-show-faces="false" data-send="false"></div>
             </div>
             <h6><?php echo $video->title; ?></h6>
-            <iframe width="654" height="368" src="<?php echo $video->url; ?>" frameborder="0"></iframe>
-            <?php echo $video->description; ?>
+            <iframe width="654" height="368" src="<?php echo $url; ?>" frameborder="0"></iframe>
+<?php echo $video->description; ?>
         </div>
         <div class="comment">
             <div class="fb-comments" data-href="<?php echo Yii::app()->request->getBaseUrl(true) . '#fb-comment-' . $video->id; ?>"></div>

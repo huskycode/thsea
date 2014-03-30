01aa8d3370e67eb104f1074f743c47d27a40378c
Narathip Harijiratiwong <tholop@gmail.com>
2014-03-30 19:32:18 +0700

    add twitter share button

--- public/protected/views/site/detail.php
+++ public/protected/views/site/detail.php
@@ -20,6 +20,16 @@ $this->currentUrl = VideoService::getVideoDetailUrl($video);
 <div id="video-detail" class="container" >
     <div class="image-post video">
         <div style="float:right;" class="pull-right">
+            <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $this->currentUrl; ?>" data-text="<?php echo $video->title; ?>">Tweet</a>
+            <script>!function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = p + '://platform.twitter.com/widgets.js';
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, 'script', 'twitter-wjs');</script>
             <div class="fb-like" data-href="<?php echo VideoService::getVideoLikeUrl($video); ?>" data-width="200" data-layout="button_count" data-show-faces="false" data-send="false"></div>
         </div>
         <h6><?php echo $video->title; ?></h6>

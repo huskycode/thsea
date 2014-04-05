6e502e54cd5c03bba48d4007035c04ef85cda129
ruxcom <ruxcom@hotmail.com>
2014-04-06 01:14:02 +0700

    Add VideoTest with fixture.

---
+++ public/protected/tests/fixtures/video.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	array(
+            'id'=>4143,
+            'title'=>'[ATH2013] วิกฤติโปรแกรมเมอร์ไทย แก้ได้ด้วยอไจล์',
+            'description'=>'จาก "แบไต๋ไฮเทค: Special Report - วิกฤตการณ์โปรแกรมเมอร์ไทย" ได้เกิดการถกเถียงกันอย่างมากใน Facebook Group ของ Agile66 ถึงสาเหตุที่แท้จริงของปัญหา รวมไปถึงหนทางที่จะแก้ไข เราจึงพูดคุยกันว่าเราจะเอาเรื่องนี้มาคุย?กันใน กิจกรรมประจำปีของชาว Agile ที่มีชื่อว่า Agile Thailand 2013 ทำให้เกิดเป็น Panel Discussion Session ดราม่าแห่งปี ในชื่อ "วิกฤติโปรแกรมเมอร์ไทย แก้ได้ด้วยอไจล์"',
+            'url'=>'http://youtu.be/gVaRj5GgOPg',
+            'url_name'=>'programmer-crisis-solved-with-agile',
+            'thumbnail_url'=>'https://i1.ytimg.com/vi/gVaRj5GgOPg/hqdefault.jpg',
+            'recording_date'=>'2014-04-05',
+            'posted_date'=>'2014-04-05',
+            'posted_by'=>'admin',
+            'view_counter'=>1
+	),
+);
---
+++ public/protected/tests/unit/VideoFixtureTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of VideoFixtureTest
+ *
+ * @author Rux
+ */
+class VideoFixtureTest extends CDbTestCase {
+    public $fixtures = array(
+            'videos'=>'Video'
+    );
+    
+    public function testgetVideo_ByExistingId_ShouldReturnThatVideo(){        
+        $video =  VideoService::getVideo(4143);
+        $this->assertEquals(4143, $video->id);
+    }
+    
+    public function testaddNewVideo_IfDuplicateUrlName_ShouldSaveFail(){
+        $newVideo = new Video();
+        $newVideo->title = "test title";
+        $newVideo->description = "test desc";
+        $newVideo->url = "http://youtube.com";
+        $newVideo->url_name = "programmer-crisis-solved-with-agile";
+        $newVideo->thumbnail_url = null;
+        $newVideo->recording_date = "2014-04-05";
+        $newVideo->posted_date = "2014-04-05";
+        $newVideo->posted_by = "admin";
+        $newVideo->view_counter = 1;
+        
+        $canSave = $newVideo->save();        
+        $this->assertFalse($canSave);
+    }
+}
+
+?>

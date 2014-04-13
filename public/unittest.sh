4f19d469df5076da8cfcf6b5821dcf0dea37bb15
Sunai Sukanake <sunai.sukanake@gmail.com>
2014-04-13 12:28:11 +0700

    fix unit test running path

--- public/unittest.sh
+++ public/unittest.sh
@@ -2,6 +2,6 @@
 ##  writable "runtime&assets" folder
 chmod -R 777 ./protected/runtime
 chmod -R 777 ./assets
-chmod -R 777 ./protected/tests
+cd /protected/tests
 ##  run phpunit
-./protected/tests phpunit unit/SiteControllerTest.php
+phpunit unit/SiteControllerTest.php

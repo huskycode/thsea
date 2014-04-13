a9d03455f10ae9b62399a26b9b14c1863ae4c0f9
Sunai Sukanake <sunai.sukanake@gmail.com>
2014-04-13 12:12:06 +0700

    fix permission on test folder

--- public/unittest.sh
+++ public/unittest.sh
@@ -2,5 +2,6 @@
 ##  writable "runtime&assets" folder
 chmod -R 777 ./protected/runtime
 chmod -R 777 ./assets
+chmod -R 777 ./protected/tests
 ##  run phpunit
 ./protected/tests phpunit unit/SiteControllerTest.php

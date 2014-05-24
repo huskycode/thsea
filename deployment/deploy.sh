eeeab2fe979451f7ed69b553202cb4ac2612e8af
ubinix-warun <warun@ubinix.com>
2014-05-24 19:43:42 +0700

    force push '.htaccess' to /home/public with $DEST var on deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,9 +1,9 @@
 ## intial destination
 if [ "$1" = "uat" ]
-then 
-     DESTINATION="varokas_thsea-uat@ssh.phx.nearlyfreespeech.net:/home/public/"   
-elif [ "$1" = "prod" ] 
-then 
+then
+     DESTINATION="varokas_thsea-uat@ssh.phx.nearlyfreespeech.net:/home/public/"
+elif [ "$1" = "prod" ]
+then
      DESTINATION="varokas_thsea@ssh.phx.nearlyfreespeech.net:/home/public/"
 else
      echo "wrong command"
@@ -14,3 +14,4 @@ BUILDPATH="$2""/public/*"
 
 ## deploy by rsync
 rsync -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/thsea/.ssh/id_rsa" -vramlHP --exclude '*.log' --exclude 'protected/config/db.php' --exclude 'protected/config/test.php' --numeric-ids --delete --delete-after --delay-updates $BUILDPATH $DESTINATION
+scp "$2""/public/.htaccess $DESTINATION
